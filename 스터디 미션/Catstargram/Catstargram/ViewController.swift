f4b5e83ee369009a983d850fce41395ba4916b85
jehunpfh <jehunpfh@gmail.com>
2023-10-04 20:19:17 +0900

    2주차 스터디 미션

---
+++ 스터디 미션/Catstargram/Catstargram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Catstargram
+//
+//  Created by 김제훈 on 2023/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
